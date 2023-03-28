dfbafb319dbbbe783fae266b83052bb3df49fe0d
hernandiaz94 <hernan.diiaz@gmail.com>
2023-03-28 20:14:07 -0300

    retoques placa 3 v2

--- proyect.php
+++ proyect.php
@@ -1,19 +1,27 @@
             <div class="container">
 
-                <div class="haveAproject" style="padding-right: 85px; padding-top: 40px">
-                    <div class="row d-flex align-items-center">
-                        <div class="col-xl-7 col-lg-9 col-md-6">
-                            <div class="wantToWork-caption" >
-                                <h2 style="color: #000; -webkit-text-stroke:  0.5px #fff;">Tenés algún proyecto en mente?</h2>
-                                <p style="color: #000;">No dudes en dejar tu mensaje.</p>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-3 col-md-6">
-                            <div class="wantToWork-btn f-left" style="margin-left: 140; margin-top:30">
+                <div class="haveAproject" style="padding-right: 85px; padding-top: 40px; margin-left: 100px;">
+                    <div class="row d-flex align-items-right">
+
+                        <div class="col-xl-8 col-lg-6 col-md-4">
+                            <div class="wantToWork-btn f-right">
                                 <a href="contact.php" class="btn hero-btn">Contactate</a>
                             </div>
                         </div>
+                        <div class="col-xl-4 col-lg-8 col-md-8">
+                        <div class="wantToWork-caption" style="margin-right: 20px;">
+                            <h2 style="color: #000; -webkit-text-stroke:  0.5px #fff; margin-left: 0px;">Tenés algún proyecto en mente?</h2>
+                            <p style="color: #000; -webkit-text-stroke:  0.1px #fff; margin-left: 0px;">No dudes en dejar tu mensaje.</p>
+                        </div>
                     </div>
+                    </div>
+                   <!--  <div class="col-xl-12 col-lg-8 col-md-6">
+                        <div class="wantToWork-caption">
+                            <h2 style="color: #000; -webkit-text-stroke:  0.5px #fff; margin-left: 400px;">Tenés algún proyecto en mente?</h2>
+                            <p style="color: #000; -webkit-text-stroke:  0.1px #fff; margin-left: 400px;">No dudes en dejar tu mensaje.</p>
+                        </div>
+                    </div> -->
                 </div>
+
 
             </div>
